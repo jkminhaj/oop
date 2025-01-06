5583e94bf2ee37b1f95160d9d0c6c8e147a74ca9
Minhajul Islam <mmminhaj221@gmail.com>
2025-01-06 21:50:06 +0600

    Update methods.py

--- methods.py
+++ methods.py
@@ -1,1 +1,26 @@
-# fu
+# function inside the class is called method
+
+# pass self keyword in all methods as default in python only
+# if the parameter is empty then will show error
+
+class Phone :
+    # These are attributes
+    name = "Samsung"
+    ram = 8
+    color = "blue"
+    
+    def ring(self) :
+        print("phone is ringing")
+        # use return or pass 
+    
+    def sendSMS(self , num , text):
+        print(f"{text} from  :{num}")
+
+# calling new : use parenthesis to make new
+
+myPhone = Phone()
+
+print(myPhone.name)
+myPhone.ring()
+
+myPhone.sendSMS(1314747,"hi")
