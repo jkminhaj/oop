df6f35f3096cc64d062d1376b8361bb3e2084a08
Minhajul Islam <mmminhaj221@gmail.com>
2025-01-09 15:04:11 +0600

    Create inheritance_concepts.py

---
+++ Inheritance/inheritance_concepts.py
@@ -0,0 +1,14 @@
+# Base class / Parent class
+class BaseClass :
+  pass
+
+# Derived class / Children class
+class DerivedClass (BaseClass) :
+  pass
+
+
+"""
+simple inheritance : parent -> children
+multilevel inheritance : granparent -> parent -> chlidren
+
+"""
