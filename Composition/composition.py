4e05799331d3c567c77bbc3b7a4d1bf7ecfa4b30
Minhajul Islam <mmminhaj221@gmail.com>
2025-01-16 15:56:19 +0600

    Update composition.py

--- Composition/composition.py
+++ Composition/composition.py
@@ -1,1 +1,22 @@
+# is a  :  Inheritance realtion
+# has a  : Composition realtion
 
+class Ram :
+    def __init__(self,ram) :
+        self.capacity = ram
+    
+
+class Hdd :
+    def __init__(self, storage) :
+        self.capacity = storage
+
+
+# computer has a ram
+class Computer : 
+    def __init__(self , ram , storage ) :
+        self.Ram = Ram(ram)
+        self.Hdd = Hdd(storage) 
+
+pc = Computer(16,200)
+
+print(pc.Ram)
